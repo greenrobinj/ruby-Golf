1097231792ad5bc39b7baee94a5e0dc99d829d8e
Robin Green <greenrj4@gmail.com>
2018-05-22 10:41:51 -0600

    hole 3

---
+++ golf.rb
@@ -0,0 +1,71 @@
+
+# # hole 1 sum an array
+# def sum(x,y)
+#    return (x + y)
+# end
+# puts sum 5,5
+
+# # hole 2 Rock paper scissors
+# def rock 
+#   comp = ['r', 'p', 's'].sample
+#   me = gets.strip
+#   if
+#     me == 'r' && comp == 's'||
+#     me == 'p' && comp == 'r'||
+#     me == 's' && comp == 'p'
+#     puts 'W'
+#   elsif 
+#     me == comp
+#     puts 'D'
+#   else
+#     puts 'L'
+#   end 
+# end
+# rock
+
+# # hole 3 fizzBuzz
+
+# def fizzBuzz
+#   (1..100).each do |i|   
+#   if 
+#     0 == i % 15
+#     puts 'FizzBuzz'
+#   elsif
+#     0 == i % 3
+#     puts  ' Buzz'
+#   elsif
+#     0 ==  i % 5
+#     puts  'Fizz'
+#   else
+#     puts i
+#   end
+#   end
+# end
+#  fizzBuzz
+
+
+
+#hole 4 multiples up to a given value
+
+def rang
+  x=2 
+  y=10
+while x <= y do 
+    i = x * (x +1)
+    puts i 
+ end
+end
+
+rang
+
+
+
+# hole 5 caesar cipher
+
+# hole 6 string conter
+
+# hole 7mixed pairs
+
+# hole 8 love test
+
+# hole 9 shoppin list
